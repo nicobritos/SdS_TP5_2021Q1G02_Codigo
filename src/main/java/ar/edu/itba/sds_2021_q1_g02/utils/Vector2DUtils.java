a44afaa9cb6de7ca22f2e97f0078a20492a95380
nicbritos <nicbritos@hotmail.com>
2021-06-05 04:16:24 -0300

    Fixeado algunos NaN al dividir

--- src/main/java/ar/edu/itba/sds_2021_q1_g02/utils/Vector2DUtils.java
+++ src/main/java/ar/edu/itba/sds_2021_q1_g02/utils/Vector2DUtils.java
@@ -14,14 +14,17 @@ public class Vector2DUtils {
         final double dvb = vertex.distanceTo(positionB);
         final double dab = positionA.distanceTo(positionB);
 
-        final double angle = Math.acos((Math.pow(dva, 2) + Math.pow(dvb, 2) - Math.pow(dab, 2)) / (2 * dva * dvb));
+        double div = 2 * dva * dvb;
+        if (div == 0)
+            div = 1;
+        final double angle = Math.acos((Math.pow(dva, 2) + Math.pow(dvb, 2) - Math.pow(dab, 2)) / div);
         return angle * 180 / Math.PI;
     }
 
     public static Vector2D calculateVectorFromTwoPositions(Position positionA, Position positionB) {
         final double x = positionA.getX() - positionB.getX();
         final double y = positionA.getY() - positionB.getY();
-        return new Vector2D(x / Math.abs(x), y / Math.abs(y));
+        return new Vector2D(x == 0 ? 0 : (x / Math.abs(x)), y == 0 ? 0 : (y / Math.abs(y)));
     }
 
     public static Vector2D calculateNormalizedVector(Vector2D vector) {
