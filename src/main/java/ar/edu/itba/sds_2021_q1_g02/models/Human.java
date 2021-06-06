aaed24921ad8c9a5eb5d6105f278c094c55bee96
nicbritos <nicbritos@hotmail.com>
2021-06-06 15:25:46 -0300

    Fixeado Human toZombie default velocity

--- src/main/java/ar/edu/itba/sds_2021_q1_g02/models/Human.java
+++ src/main/java/ar/edu/itba/sds_2021_q1_g02/models/Human.java
@@ -2,7 +2,6 @@ package ar.edu.itba.sds_2021_q1_g02.models;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Human extends Particle {
     private final Set<Zombie> chasedBy;
@@ -25,7 +24,7 @@ public class Human extends Particle {
 
     public Zombie toZombie(ParticleZone particleZone, double radius) {
         this.zombie = true;
-        return new Zombie(this.getId(), this.getRadius(), this.getPosition(), Velocity.ZERO);
+        return new Zombie(this.getId(), particleZone, radius, this.getPosition(), Velocity.ZERO);
     }
 
     public void addChasingZombie(Zombie zombie) {
@@ -42,7 +41,7 @@ public class Human extends Particle {
 
     @Override
     protected Particle childCopy() {
-        Human human = new Human(this.getId(), this.getRadius(), this.getPosition(), this.getVelocity());
+        Human human = new Human(this.getId(), this.getParticleZone(), this.getRadius(), this.getPosition(), this.getVelocity());
         human.setType(Type.BITTEN_HUMAN);
         return human;
     }
